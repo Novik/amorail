c4af88ce1644dceed49a460fe4890d26f9dab998
Novik <novik65@gmail.com>
2018-07-13 17:49:14 +0300

    add visitor field

--- lib/amorail/entities/lead.rb
+++ lib/amorail/entities/lead.rb
@@ -3,7 +3,7 @@ module Amorail
   class Lead < Amorail::Entity
     amo_names "leads"
 
-    amo_field :name, :price, :status_id, :pipeline_id, :tags
+    amo_field :name, :price, :status_id, :pipeline_id, :tags, :visitor_uid
 
     validates :name, :status_id, presence: true
 
